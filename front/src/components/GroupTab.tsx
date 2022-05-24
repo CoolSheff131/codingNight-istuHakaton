3167a96a909041b516ee68715eb8bc3db479ee16
Александр Никульшеев <a.nikulsheev@gmail.com>
2022-05-24 17:10:25 +0800

    Переделана вкладка групп

--- front/src/components/GroupTab.tsx
+++ front/src/components/GroupTab.tsx
@@ -1,196 +1,236 @@
-import { Box, Button, Typography } from '@mui/material';
+import {
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Box,
+  Button,
+  Typography,
+} from '@mui/material';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import ColorButton from './ColorButton';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
 
 const GroupTab = () => {
+  const [selectedInstitute, setSelectedInstitute] = React.useState<
+    number | null
+  >(null);
   return (
     <Box>
       <Typography sx={{ color: '#7165E3', fontSize: 18, marginTop: '14px' }}>
-        Институт
+        {selectedInstitute === null
+          ? 'Институт'
+          : 'Институт Информационных Технологий и Анализа Данных'}
       </Typography>
 
       <Box
         sx={{
           display: 'flex',
-          flexWrap: 'wrap',
+          marginTop: '21px',
+          flexDirection: 'column',
         }}
       >
-        {Array.from(Array(10).keys()).map((number) => {
-          return (
-            <ColorButton key={number} variant="contained">
-              ИИТиАД
-            </ColorButton>
-          );
-        })}
-      </Box>
-
-      <Typography
-        sx={{
-          fontWeight: 500,
-          color: '#000',
-          fontSize: 18,
-          marginTop: '32px',
-        }}
-      >
-        Первый курс
-      </Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'start',
-        }}
-      >
-        {Array.from(Array(10).keys()).map((number) => {
-          return (
-            <NavLink
-              key={number}
-              to={`${number}`}
-              style={{ textDecoration: 'none' }}
-            >
+        {selectedInstitute === null &&
+          Array.from(Array(10).keys()).map((number) => {
+            return (
               <Button
-                variant="text"
+                onClick={() => setSelectedInstitute(number)}
+                key={number}
+                variant="contained"
                 sx={{
-                  padding: 0,
-                  textTransform: 'none',
-                  fontWeight: 400,
-                  color: '#3B3D48',
-                  fontSize: 16,
-                  marginTop: '7px',
-                  marginRight: '10px',
+                  '&:hover': { backgroundColor: '#7165E3', color: '#E8E6FF' },
+                  marginTop: '9px',
+                  textAlign: 'left',
+                  width: '100%',
+                  backgroundColor: '#E8E6FF',
+                  color: '#7165E3',
+                  fontSize: '18px',
                 }}
               >
-                ИСТб-19-1
+                Институт Информационных Технологий и Анализа Данных
               </Button>
-            </NavLink>
-          );
-        })}
-      </Box>
-      <Typography
-        sx={{
-          fontWeight: 500,
-          color: '#000',
-          fontSize: 18,
-          marginTop: '32px',
-        }}
-      >
-        Второй курс
-      </Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'start',
-        }}
-      >
-        {Array.from(Array(10).keys()).map((number) => {
-          return (
-            <NavLink
-              key={number}
-              to={`${number}`}
-              style={{ textDecoration: 'none' }}
-            >
-              <Button
-                variant="text"
-                sx={{
-                  padding: 0,
-                  textTransform: 'none',
-                  fontWeight: 400,
-                  color: '#3B3D48',
-                  fontSize: 16,
-                  marginTop: '7px',
-                  marginRight: '10px',
-                }}
+            );
+          })}
+        {selectedInstitute !== null && (
+          <>
+            <Accordion sx={{ borderRadius: '5px', marginBottom: '9px' }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
               >
-                ИСТб-19-1
-              </Button>
-            </NavLink>
-          );
-        })}
-      </Box>
-      <Typography
-        sx={{
-          fontWeight: 500,
-          color: '#000',
-          fontSize: 18,
-          marginTop: '32px',
-        }}
-      >
-        Третий курс
-      </Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'start',
-        }}
-      >
-        {Array.from(Array(10).keys()).map((number) => {
-          return (
-            <NavLink
-              key={number}
-              to={`${number}`}
-              style={{ textDecoration: 'none' }}
-            >
-              <Button
-                variant="text"
-                sx={{
-                  padding: 0,
-                  textTransform: 'none',
-                  fontWeight: 400,
-                  color: '#3B3D48',
-                  fontSize: 16,
-                  marginTop: '7px',
-                  marginRight: '10px',
-                }}
+                <Typography sx={{ color: '#3B3D48', fontSize: '18px' }}>
+                  1 курс
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                  {Array.from(Array(10).keys()).map((number) => {
+                    return (
+                      <NavLink
+                        key={number}
+                        to={`${number}`}
+                        style={{ textDecoration: 'none' }}
+                      >
+                        <Button
+                          variant="outlined"
+                          sx={{
+                            paddingTop: '10px',
+                            paddingBottom: '10px',
+                            paddingLeft: '15px',
+                            paddingRight: '15px',
+                            borderRadius: '10px',
+                            borderColor: '#8B80F8',
+                            textTransform: 'none',
+                            fontWeight: 400,
+                            color: '#3C30AC',
+                            fontSize: 18,
+                            marginTop: '7px',
+                            marginRight: '10px',
+                          }}
+                        >
+                          ИСТб-19-1
+                        </Button>
+                      </NavLink>
+                    );
+                  })}
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{ borderRadius: '5px', marginBottom: '9px' }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
               >
-                ИСТб-19-1
-              </Button>
-            </NavLink>
-          );
-        })}
-      </Box>
-      <Typography
-        sx={{
-          fontWeight: 500,
-          color: '#000',
-          fontSize: 18,
-          marginTop: '32px',
-        }}
-      >
-        Четвертый курс
-      </Typography>
-      <Box
-        sx={{
-          display: 'flex',
-          flexWrap: 'wrap',
-          justifyContent: 'start',
-        }}
-      >
-        {Array.from(Array(10).keys()).map((number) => {
-          return (
-            <NavLink
-              key={number}
-              to={`${number}`}
-              style={{ textDecoration: 'none' }}
-            >
-              <Button
-                variant="text"
-                sx={{
-                  padding: 0,
-                  textTransform: 'none',
-                  fontWeight: 400,
-                  color: '#3B3D48',
-                  fontSize: 16,
-                  marginTop: '7px',
-                  marginRight: '10px',
-                }}
+                <Typography sx={{ color: '#3B3D48', fontSize: '18px' }}>
+                  2 курс
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                  {Array.from(Array(10).keys()).map((number) => {
+                    return (
+                      <NavLink
+                        key={number}
+                        to={`${number}`}
+                        style={{ textDecoration: 'none' }}
+                      >
+                        <Button
+                          variant="outlined"
+                          sx={{
+                            paddingTop: '10px',
+                            paddingBottom: '10px',
+                            paddingLeft: '15px',
+                            paddingRight: '15px',
+                            borderRadius: '10px',
+                            borderColor: '#8B80F8',
+                            textTransform: 'none',
+                            fontWeight: 400,
+                            color: '#3C30AC',
+                            fontSize: 18,
+                            marginTop: '7px',
+                            marginRight: '10px',
+                          }}
+                        >
+                          ИСТб-19-1
+                        </Button>
+                      </NavLink>
+                    );
+                  })}
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{ borderRadius: '5px', marginBottom: '9px' }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
               >
-                ИСТб-19-1
-              </Button>
-            </NavLink>
-          );
-        })}
+                <Typography sx={{ color: '#3B3D48', fontSize: '18px' }}>
+                  3 курс
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                  {Array.from(Array(10).keys()).map((number) => {
+                    return (
+                      <NavLink
+                        key={number}
+                        to={`${number}`}
+                        style={{ textDecoration: 'none' }}
+                      >
+                        <Button
+                          variant="outlined"
+                          sx={{
+                            paddingTop: '10px',
+                            paddingBottom: '10px',
+                            paddingLeft: '15px',
+                            paddingRight: '15px',
+                            borderRadius: '10px',
+                            borderColor: '#8B80F8',
+                            textTransform: 'none',
+                            fontWeight: 400,
+                            color: '#3C30AC',
+                            fontSize: 18,
+                            marginTop: '7px',
+                            marginRight: '10px',
+                          }}
+                        >
+                          ИСТб-19-1
+                        </Button>
+                      </NavLink>
+                    );
+                  })}
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+            <Accordion sx={{ borderRadius: '5px', marginBottom: '9px' }}>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
+              >
+                <Typography sx={{ color: '#3B3D48', fontSize: '18px' }}>
+                  4 курс
+                </Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+                  {Array.from(Array(10).keys()).map((number) => {
+                    return (
+                      <NavLink
+                        key={number}
+                        to={`${number}`}
+                        style={{ textDecoration: 'none' }}
+                      >
+                        <Button
+                          variant="outlined"
+                          sx={{
+                            paddingTop: '10px',
+                            paddingBottom: '10px',
+                            paddingLeft: '15px',
+                            paddingRight: '15px',
+                            borderRadius: '10px',
+                            borderColor: '#8B80F8',
+                            textTransform: 'none',
+                            fontWeight: 400,
+                            color: '#3C30AC',
+                            fontSize: 18,
+                            marginTop: '7px',
+                            marginRight: '10px',
+                          }}
+                        >
+                          ИСТб-19-1
+                        </Button>
+                      </NavLink>
+                    );
+                  })}
+                </Box>
+              </AccordionDetails>
+            </Accordion>
+          </>
+        )}
       </Box>
     </Box>
   );
